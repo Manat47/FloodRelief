5812ff4c58f1e4d5013e7b0ad9984c667e5458cc
KhanShelby <afkhan_1@hotmail.com>
2025-02-28 11:36:59 +0700

    #1

---
+++ app/(taps)/YourCase.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function YourCase() {
+  return (
+    <View>
+      <Text>YourCase</Text>
+    </View>
+  )
+}
---
+++ app/(taps)/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+export default function TabLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen name ='Home' />
+            <Tabs.Screen name ='Request' />
+            <Tabs.Screen name ='YourCase' />
+            <Tabs.Screen name ='Profile' />
+        </Tabs>
+    )
+}
